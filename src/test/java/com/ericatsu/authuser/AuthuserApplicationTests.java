5576ad311dcc7697126d466a29f9abb7a5866e98
ericatsu <ekaatsu@st.ug.edu.gh>
2023-06-15 18:51:21 +0000

    init

---
+++ src/test/java/com/ericatsu/authuser/AuthuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericatsu.authuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
