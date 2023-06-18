bcca0992dd1000cd68912de21f81edaddb47fe1d
ericatsu <ekaatsu@st.ug.edu.gh>
2023-06-18 23:32:06 +0000

    🥅 HttpSecurity

--- src/main/java/com/ericatsu/authuser/security/PasswordEncoder.java
+++ src/main/java/com/ericatsu/authuser/security/PasswordEncoder.java
@@ -1,5 +1,14 @@
 package com.ericatsu.authuser.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
 public class PasswordEncoder {
     
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/ericatsu/authuser/security/config/WebSecurityConfig.java
+++ src/main/java/com/ericatsu/authuser/security/config/WebSecurityConfig.java
@@ -1,20 +1,49 @@
 package com.ericatsu.authuser.security.config;
 
-import org.hibernate.jdbc.Expectations;
+import com.ericatsu.authuser.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter  {
-    
-    @Override
-    protected void configure(HttpSecurity http) throws Expectations {
-        super.configure(http);
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringRequestMatchers("/api/v*/registration/**")
+                .and()
+                .authorizeRequests()
+                .requestMatchers("/api/v*/registration/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
     }
-    
+
+    @Bean
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
