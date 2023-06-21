e78a0c049b814a466e3721484a71e3d5381e1be9
ericatsu <ekaatsu@st.ug.edu.gh>
2023-06-21 19:05:49 +0000

    bean error

--- src/main/java/com/ericatsu/authuser/security/config/WebSecurityConfig.java
+++ src/main/java/com/ericatsu/authuser/security/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @AllArgsConstructor
@@ -19,14 +20,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Bean
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().ignoringRequestMatchers("/api/v*/registration/**")
-                .and()
-                .authorizeRequests()
-                .requestMatchers("/api/v*/registration/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authz -> authz
+        .requestMatchers("/api/v*/registration/**").permitAll()
+                .anyRequest()
+                .authenticated());
+
+                return http.build();
     }
 
     @Bean
@@ -42,8 +42,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return provider;
     }
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    // @Bean
+    // public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    //     return new BCryptPasswordEncoder();
+    // }
 }
